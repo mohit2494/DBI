fab2cf69eb06fd39cd4fe68c6d09cac4ea0c9174
ksathyasairam <sattyboy123@gmail.com>
2020-02-10 21:18:12 -0500

    Temp Code Design

--- P2_P1/BigQ.cc
+++ P2_P1/BigQ.cc
@@ -1,7 +1,48 @@
 #include "BigQ.h"
 
+BigQ :: Driver(){
+    Phase1();
+    getMemoryConfig();
+    Phase2();
+}
+
+BigQ :: Phase1()
+{
+    Run myRun(myThreadData.runlen);
+    Record readBuffer;
+    DBFile tempFile;
+    while(myThreadData.in->remove(&readBuffer)){
+        myRun.addRecord(&readBuffer);
+    }
+    myRun.sort();
+    tempFile.Open();
+    myRun.writeSortedRecords(&DBFile);
+    tempFile
+    
+}
+
+BigQ :: Phase2()
+{
+    Run myRun(myThreadData.runlen);
+    Record readBuffer;
+    DBFile tempFile;
+    while(myThreadData.in->remove(&readBuffer)){
+        myRun.addRecord(&readBuffer);
+    }
+    myRun.sort();
+    tempFile.Open();
+    myRun.writeSortedRecords(&DBFile);
+    tempFile
+    
+}
+
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 	// read data from in pipe sort them into runlen pages
+    myThreadData.in = in;
+    myThreadData.out = out;
+    myThreadData.sortorder = sortorder;
+    myThreadData.runlen = runlen;
+    pthread_create (&myThread, NULL, Driver);
 
     // construct priority queue over sorted runs and dump sorted data 
  	// into the out pipe
--- P2_P1/BigQ.h
+++ P2_P1/BigQ.h
@@ -8,12 +8,44 @@
 
 using namespace std;
 
+typedef struct {
+    Pipe * in;
+    Pipe * out;
+    OrderMaker * sortOrder;
+    int runlen;
+} ThreadData;
+
+class TournamentTree{
+    
+};
+
+class Runs{
+    
+public:
+    AddPage();
+    SortRun();
+    RemovePage();
+    
+};
+
+class RunManager{
+    
+    
+};
+
 class BigQ {
+    ThreadData myThreadData;
+    TournamentTree myTree;
+    pthread_t myThread;
+    int totalRuns;
+    bool isLastRunComplete;
+    DbFile myFile;
 
 public:
 
 	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
 	~BigQ ();
+    Driver(void *arg);
 };
 
 #endif
