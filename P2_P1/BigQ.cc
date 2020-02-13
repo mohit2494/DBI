fcfbf7b030f5eeb005b367b1a892419ec0fce59b
ksathyasairam <sattyboy123@gmail.com>
2020-02-13 00:20:00 -0500

    Commit 1

--- P2_P1/BigQ.cc
+++ P2_P1/BigQ.cc
@@ -1,54 +1,102 @@
 #include "BigQ.h"
 
-BigQ :: Driver(){
-    Phase1();
-    getMemoryConfig();
-    Phase2();
+
+TreeComparator :: TreeComparator(OrderMaker &sortorder){
+    myOrderMaker = &sortorder;
 }
 
-BigQ :: Phase1()
-{
-    Run myRun(myThreadData.runlen);
-    Record readBuffer;
-    DBFile tempFile;
-    while(myThreadData.in->remove(&readBuffer)){
-        myRun.addRecord(&readBuffer);
-    }
-    myRun.sort();
-    tempFile.Open();
-    myRun.writeSortedRecords(&DBFile);
-    tempFile
+
+Run::Run(int runlen) {
+    this->runLength = runlen;
+}
+void Run::AddPage(Page p) {
+    this->pages.push_back(p);
+}
+void Run::sortRun() {
+    // sort pages
+    // insert pages into tournament
+}
+vector<Page> Run::getPages() {
+    return this->pages;
+}
+bool Run::checkRunFull() {
+    return this->pages.size() == this->runLength;
+}
+bool Run::clearPages() {
+    this->pages.clear();
+}
+int Run::getRunSize() {
+    return this->pages.size();
+}
+
+TournamentTree :: TournamentTree(OrderMaker &sortorder,Run &run){
+    myOrderMaker = &sortorder;
+    myQueue = new priority_queue<Record,vector<Record>,TreeComparator>(TreeComparator(sortorder));
+    isRunManagerAvailable = false;
+    Inititate(run.getPages());
     
 }
 
-BigQ :: Phase2()
-{
-    Run myRun(myThreadData.runlen);
-    Record readBuffer;
-    DBFile tempFile;
-    while(myThreadData.in->remove(&readBuffer)){
-        myRun.addRecord(&readBuffer);
-    }
-    myRun.sort();
-    tempFile.Open();
-    myRun.writeSortedRecords(&DBFile);
-    tempFile
+TournamentTree :: TournamentTree(OrderMaker &sortorder,RunManager &manager){
+    myOrderMaker = &sortorder;
+    myRunManager = &manager;
+    myQueue = new priority_queue<Record,vector<Record>,TreeComparator>(TreeComparator(sortorder));
+    isRunManagerAvailable = true
+    Inititate(myRunManager.getPages());
+
+}
+void TournamentTree :: Inititate(vector<Page> pages){
     
+    for(vector<Page>::iterator page = pages.begin() ; i!=pages.end();++page){
+        Record tempRecord;
+        page
+        
+        myQueue->push(
+    }
+    
+}
+
+void* BigQ :: Driver(void *p){
+    BigQ * ptr = (BigQ*) p;
+    ptr->Phase1();
+    ptr->Phase2();
+}
+
+void BigQ :: Inititate(){
+    
+}
+void BigQ :: Phase1()
+{
+    
+    
+}
+
+void BigQ :: Phase2()
+{
+    cout <<"Phase2";
+    
+    //    Run myRun(myThreadData.runlen);
+    //    Record readBuffer;
+    //    DBFile tempFile;
+    //    while(myThreadData.in->remove(&readBuffer)){
+    //        myRun.addRecord(&readBuffer);
+    //    }
+    //    myRun.sort();
+    //    tempFile.Open();
+    //    myRun.writeSortedRecords(&DBFile);
+    //    tempFile
+    //
 }
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
-	// read data from in pipe sort them into runlen pages
-    myThreadData.in = in;
-    myThreadData.out = out;
-    myThreadData.sortorder = sortorder;
+    myThreadData.in = &in;
+    myThreadData.out = &out;
+    myThreadData.sortorder = &sortorder;
     myThreadData.runlen = runlen;
-    pthread_create (&myThread, NULL, Driver);
-
-    // construct priority queue over sorted runs and dump sorted data 
- 	// into the out pipe
-
-    // finally shut down the out pipe
-	out.ShutDown ();
+    myTree = new TournamentTree(sortorder);
+    pthread_create(&myThread, NULL, BigQ::Driver,this);
+    pthread_join(myThread,NULL);
+    out.ShutDown ();
 }
 
 BigQ::~BigQ () {
--- P2_P1/BigQ.h
+++ P2_P1/BigQ.h
@@ -5,47 +5,85 @@
 #include "Pipe.h"
 #include "File.h"
 #include "Record.h"
-
+#include <vector>
+#include <queue>
+#include "ComparisonEngine.h"
+#include "DBFile.h"
 using namespace std;
+
 
 typedef struct {
     Pipe * in;
     Pipe * out;
-    OrderMaker * sortOrder;
+    OrderMaker * sortorder;
     int runlen;
 } ThreadData;
 
-class TournamentTree{
-    
-};
 
-class Runs{
-    
+class TreeComparator{
+    ComparisonEngine myComparisonEngine;
+    OrderMaker * myOrderMaker;
 public:
-    AddPage();
-    SortRun();
-    RemovePage();
-    
+    TreeComparator(OrderMaker &sortorder);
+    bool operator()(const Record & lhs, const Record &rhs);
 };
 
 class RunManager{
-    
-    
+    DBFile * myFile;
+    int noOfRuns;
+    int runLength;
+    vector <Page> pages;
+    vector <int> fileDescriptors;
+public:
+    vector<Page *> getPagesForSorting();
+    Page * getNextPageOfRun(int runNo,int pageOffset);
 };
+
+
+class Run {
+    int runLength;
+    vector <Page> pages;
+    public:
+        Run(int runLength);
+        void AddPage(Page p);
+        void sortRun();
+        bool checkRunFull();
+        bool clearPages();
+        int getRunSize();
+        vector<Page> getPages();
+};
+
+
+class TournamentTree{
+    OrderMaker * myOrderMaker = NULL;
+    RunManager * myRunManager = NULL;
+    Page OutputBuffer;
+    bool isRunManagerAvailable;
+    priority_queue<Record,vector<Record>,TreeComparator> * myQueue;
+    void Inititate(vector<Page> pages);
+public:
+    TournamentTree(OrderMaker &sortorder,RunManager &manager);
+    bool GetSortedPage(&Page);
+};
+
+
+
 
 class BigQ {
     ThreadData myThreadData;
-    TournamentTree myTree;
+    TournamentTree * myTree;
     pthread_t myThread;
     int totalRuns;
     bool isLastRunComplete;
-    DbFile myFile;
+    DBFile myFile;
+    void Inititate();
+    void Phase1();
+    void Phase2();
 
 public:
-
-	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
+  BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
 	~BigQ ();
-    Driver(void *arg);
+  static void* Driver(void*);
 };
 
 #endif
